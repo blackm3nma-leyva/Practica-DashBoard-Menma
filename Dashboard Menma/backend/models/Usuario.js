73be05a2964c6129db12817a9784f0a2325f68c0
blackm3nma-leyva <blackm3nma@gmail.com>
2025-10-07 19:39:21 -0600

    Delete Dashboard Menma directory

--- Dashboard Menma/backend/models/Usuario.js
+++
@@ -1,8 +0,0 @@
-const mongoose = requiere("mongoose");
-
-const usuarioSchema = new mongoose.Schema({
-    correo: { type: String, requiered: true, unique: true },
-    clave: { type: String, requiered: true }
-});
-
-module.exports = mongoose.model("Usuario", usuarioSchema);
